638523b60c0aff2d96d86f8ca219c0fee7d0f943
zhoucunshuang <674774866@qq.com>
2020-01-17 16:21:42 +0800

    提交deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,10 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-git push -f "https://${TOKEN}@github.com/zhoucunshuang/zhoucunshuang.github.io.git" master
+#git push -f git@github.com:zhoucunshuang/zhoucunshuang.github.io.git master
+#git push -f "https://${TOKEN}@github.com/zhoucunshuang/zhoucunshuang.github.io.git" master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+ git push -f git@github.com:zhoucunshuang/blog.git master:gh-pages
 
 cd -
